191d1253878e7644b86bfa14e8d560871e7285d8
melodytng <melodytang2002@yahoo.com>
2018-07-12 17:26:30 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,7 +8,7 @@ def change_rainbow_colors
   rainbow_colors[0] = "red"
   rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
-returns rainbow_colors
+  rainbow_colors
 end
 puts change_rainbow_colors
 

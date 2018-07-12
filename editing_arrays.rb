ec70ba470209bbe560443b4309bae4599bf66e4f
melodytng <melodytang2002@yahoo.com>
2018-07-12 17:37:09 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -14,6 +14,11 @@ end
 change_rainbow_colors
 
 def add_colors
+  rainbow_colors = [
+  "yellow",
+  "default",
+  "light_cyan"
+]
   rainbow_colors.push "green"
   rainbow_colors.push "blue"
   return rainbow_colors

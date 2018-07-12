33462d21869752d2b17681db4b547fab217ce33b
melodytng <melodytang2002@yahoo.com>
2018-07-12 17:31:21 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -9,7 +9,7 @@ def change_rainbow_colors
   rainbow_colors[0] = "red"
   rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
-  return rainbow_colors
+  puts rainbow_colors
 end
 
 
